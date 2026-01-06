453988bce0124b9b9246877272d29d6568dd5fa7
Do-Van-Nam <nam15sep2003@gmail.com>
2026-01-07 00:01:40 +0700

    2

--- index.py
+++ index.py
@@ -60,5 +60,5 @@ def optimize_plan(req: OptimizeRequest):
         "status": "optimal" if prob.status == 1 else "infeasible"
     }
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8001)
